820dd0f076e24cb92698c669da37d2c2e389a3ce
ICE-BERG2 <125518912+ICE-BERG2@users.noreply.github.com>
2023-03-28 15:56:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-zmwdb9
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-zmwdb9)
---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+function sumOfElements(arr) {
+  let sum = 0;
+  for(let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+
